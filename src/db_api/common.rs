ddd425f38c3a114603d3741b45c17943cfc23832
JDSeiler <jorsei1p217@gmail.com>
2020-04-19 10:04:39 -0400

    Change path to db file to be relative from project root

--- src/db_api/common.rs
+++ src/db_api/common.rs
@@ -1,7 +1,7 @@
 use rusqlite::{Connection};
 
 pub fn get_database_connection() -> Result<Connection, rusqlite::Error> {
-    let test_db_path = "./db_storage/dummy.db";
+    let test_db_path = "./src/db_storage/dummy.db";
     let maybe_conn = Connection::open(&test_db_path);
     maybe_conn
 }
