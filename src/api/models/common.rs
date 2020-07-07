c2dc11d143454eabeea0c95cb9791dfaa683a234
JDSeiler <jorsei1p217@gmail.com>
2020-07-07 10:53:40 -0400

    Add macro for dynamic impl blocks in record structs

--- src/api/models/common.rs
+++ src/api/models/common.rs
@@ -1,6 +1,6 @@
-use rusqlite::{Connection};
+use rusqlite::Connection;
 
-/** 
+/**
 
 Returns a `Result` that is either a usable connection to the database
 or a `rusqlite::Error`. The path to the database is currently
@@ -13,4 +13,53 @@ pub fn get_database_connection() -> Result<Connection, rusqlite::Error> {
     let test_db_path = "./src/db_storage/dummy.db";
     let maybe_conn = Connection::open(&test_db_path);
     maybe_conn
+}
+
+/** 
+This is getting a gigantic doc comment because I don't understand this
+super well and documentation will help me internalize and remember.
+
+First, #[macro_export] is the attribute you have to use to make a 
+macro usable across modules.
+
+Next, this macro is "overloaded", once for non pub structs and onces for
+pub structs.
+
+Fundamentally what this does is take in some Rust code as a pattern, then
+spit out some Rust code in response. This macro takes in a struct
+definition and spits out an identical struct definition with the derive
+attributes added to it, and also creates an impl block for the field_names
+method.
+**/
+#[macro_export]
+macro_rules! create_struct_with_impl {
+    (struct $name:ident {
+        $($field_name:ident: $field_type:ty,)*
+    }) => {
+        #[derive(Serialize, Deserialize, Debug)]
+        struct $name {
+            $($field_name: $field_type,)*
+        }
+
+        impl $name {
+            fn field_names() -> Vec<&'static str> {
+                vec![$(stringify!($field_name)), *]
+            }
+        }
+    };
+
+    (pub struct $name:ident {
+        $($field_name:ident: $field_type:ty,)*
+    }) => {
+        #[derive(Serialize, Deserialize, Debug)]
+        pub struct $name {
+            $($field_name: $field_type,)*
+        }
+
+        impl $name {
+            fn field_names() -> Vec<&'static str> {
+                vec![$(stringify!($field_name)), *]
+            }
+        }
+    }
 }
