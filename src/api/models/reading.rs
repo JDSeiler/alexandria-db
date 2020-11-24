1c891f0c95d100047e8445b30092a4628d8e1b20
JDSeiler <jorsei1p217@gmail.com>
2020-11-24 13:36:54 -0500

    Add function for updating reading records

--- src/api/models/reading.rs
+++ src/api/models/reading.rs
@@ -2,21 +2,18 @@ use serde::{Deserialize, Serialize};
 
 use super::common;
 
-#[derive(Serialize, Deserialize)]
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Reading {
     id: u32,
     book: u32,
     start_date: String,
     end_date: Option<String>,
-    notes: Option<String>
+    notes: Option<String>,
 }
 
 pub fn delete_reading_by_id(id: u32) -> Result<usize, rusqlite::Error> {
     let conn = common::get_database_connection()?;
     let mut stmt = conn.prepare("DELETE FROM reading WHERE id = :id;")?;
-    // execute_named returns either Ok(usize) or Err(rusqlite::Error)
-    // which is exactly what I want, so it can be returned as is.
     stmt.execute_named(&[(":id", &id)])
 }
 
@@ -24,14 +21,35 @@ pub fn query_reading_by_id(id: u32) -> Result<Reading, rusqlite::Error> {
     let conn = common::get_database_connection()?;
     let mut stmt = conn.prepare("SELECT * FROM reading WHERE id = :id;")?;
     let row = stmt.query_row_named(&[(":id", &id)], |row| {
-	Ok(Reading{
-	    id: row.get(0)?,
-	    book: row.get(1)?,
-	    start_date: row.get(2)?,
-	    end_date: row.get(3)?,
-	    notes: row.get(4)?
-	})
+        Ok(Reading {
+            id: row.get(0)?,
+            book: row.get(1)?,
+            start_date: row.get(2)?,
+            end_date: row.get(3)?,
+            notes: row.get(4)?,
+        })
     })?;
     Ok(row)
 }
 
+pub fn update_reading_in_db(reading: Reading) -> Result<usize, rusqlite::Error> {
+    let conn = common::get_database_connection()?;
+    let mut stmt = conn.prepare(
+"UPDATE reading SET 
+book = :book,
+start_date = :start_date,
+end_date = :end_date,
+notes = :notes
+WHERE id = :id;"
+)?;
+
+    println!("{:#?}", reading);
+    let params: &[(&str, &dyn rusqlite::ToSql)] = &[
+        (":id", &reading.id),
+        (":book", &reading.book),
+        (":start_date", &reading.start_date),
+        (":end_date", &reading.end_date),
+        (":notes", &reading.notes)
+    ];
+    stmt.execute_named(params)
+}
