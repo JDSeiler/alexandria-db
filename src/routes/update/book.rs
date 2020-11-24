63ba94669041301d294d39ea168cf5a99cf8f27c
JDSeiler <jorsei1p217@gmail.com>
2020-11-24 13:35:19 -0500

    Change path for update routes

--- src/routes/update/book.rs
+++ src/routes/update/book.rs
@@ -2,17 +2,17 @@ use warp::Filter;
 use crate::api::controllers::book;
 use std::collections::HashMap;
 
+const UPDATE_ROOT: &str = "update";
 const BOOK_ROOT: &str = "book";
 
 pub fn by_id() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::path(BOOK_ROOT)
-        .and(warp::path("id"))
+    warp::path(UPDATE_ROOT)
+        .and(warp::path(BOOK_ROOT))
         .and(warp::put())
 	.and(warp::body::content_length_limit(1024 * 4))
 	.and(warp::body::json())
 	.map(|body: HashMap<String, serde_json::Value>| {
             let body = serde_json::to_string(&body).unwrap();
-            // return book::update_book_handler(id, body);
-            format!("got {}", body)
+            book::update_book_handler(body)
         })
 }
