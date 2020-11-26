6f7753ac335160b96fece3ee5d517e601f0e8e53
JDSeiler <jorsei1p217@gmail.com>
2020-11-26 11:33:03 -0500

    Split search endpoint into books and readings

--- src/routes/search/search.rs
+++ src/routes/search/search.rs
@@ -6,9 +6,6 @@ const BOOK_ROOT: &str = "books";
 const READING_ROOT: &str = "readings";
 
 /* 
-Some notes:
-The .unify makes the type signatures later in the chain nicer
-
 query() is odd, it parses query params into this enigmatic type "T".
 T is actually inferred *somehow* from the closure inside map. The type
 wizardry happening here is too much for my brain. However, query() is
@@ -18,12 +15,23 @@ that would work there is acceptable for the param in map.
 pub fn search_content() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
     let books_search = warp::path(BOOK_ROOT);
     let readings_search = warp::path(READING_ROOT);
-    warp::path(SEARCH_ROOT)
-        .and(books_search.or(readings_search).unify())
+
+    let book_search_route = warp::path(SEARCH_ROOT)
         .and(warp::get())
+        .and(books_search)
         .and(warp::query::query())
         .map(|params: HashMap<String, String>| {
             println!("Params are {:#?}", params);
-            format!("You made it to the search endpoint!")
-        })
+            format!("You made it to the book search endpoint!")
+        });
+    let readings_search_route = warp::path(SEARCH_ROOT)
+        .and(warp::get())
+        .and(readings_search)
+        .and(warp::query::query())
+        .map(|params: HashMap<String, String>| {
+            println!("Params are {:#?}", params);
+            format!("You made it to the reading search endpoint!")
+        });
+
+    book_search_route.or(readings_search_route)
 }
